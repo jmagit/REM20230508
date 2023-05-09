6c07bf162e92c17dc552d791b77570249e18d2a6
Javier <jmartin@grupoloyal.com>
2023-05-09 11:55:12 +0200

    Notificaciones

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component, Optional } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
 import { ERROR_LEVEL, LoggerService } from 'src/lib/my-core';
+import { NotificationService, NotificationType } from './common-services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Hola mundo';
 
-  constructor(@Optional() log?: LoggerService) {
-    if(!log) return;
-    log.error('Es un error')
-    log.warn('Es un warn')
-    log.info('Es un info')
-    log.log('Es un log')
+  // constructor(@Optional() log?: LoggerService) {
+  //   if(!log) return;
+  //   log.error('Es un error')
+  //   log.warn('Es un warn')
+  //   log.info('Es un info')
+  //   log.log('Es un log')
+  // }
+  constructor(private notify: NotificationService) {}
+
+  ngOnInit(): void {
+    this.notify.add('Inicio la aplicación', NotificationType.info)
   }
 }
