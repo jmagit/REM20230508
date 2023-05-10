7a114e194cb23517bec82436a8d7514bc54d6734
Javier <jmartin@grupoloyal.com>
2023-05-10 11:56:31 +0200

    dinamico

--- curso/src/app/dinamico/dinamico.component.ts
+++ curso/src/app/dinamico/dinamico.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../main';
+import { DemosComponent } from '../demos/demos.component';
 
 @Component({
   selector: 'app-dinamico',
@@ -7,6 +9,12 @@ import { Component } from '@angular/core';
 })
 export class DinamicoComponent {
   menu = [
-
+    { texto: 'demos', icono: '', componente: DemosComponent },
+    { texto: 'inicio', icono: '', componente: HomeComponent},
   ]
+  seleccionado = this.menu[0].componente
+
+  seleccionar(index: number) {
+    this.seleccionado = this.menu[index].componente
+  }
 }
