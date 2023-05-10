e8287fb995d52fd1bc8d9d1921b67c52dfc12e2f
Javier <jmartin@grupoloyal.com>
2023-05-10 11:38:25 +0200

    Pipes

---
+++ curso/src/app/dinamico/dinamico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dinamico',
+  templateUrl: './dinamico.component.html',
+  styleUrls: ['./dinamico.component.css']
+})
+export class DinamicoComponent {
+  menu = [
+
+  ]
+}
