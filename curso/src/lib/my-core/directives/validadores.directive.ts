54a080fe5296d81ac4525904c983319337661af8
Javier <jmartin@grupoloyal.com>
2023-05-11 09:37:44 +0200

    NIFValidator

---
+++ curso/src/lib/my-core/directives/validadores.directive.ts
@@ -0,0 +1,43 @@
+import { Directive, ElementRef } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn } from '@angular/forms';
+
+export function NIFValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+      if (!control.value) { return null; }
+      const err = { nif: { invalidFormat: true, invalidChar: true } };
+      if (/^\d{1,8}\w$/.test(control.value)) {
+          const letterValue = control.value.substr(control.value.length - 1);
+          const numberValue = control.value.substr(0, control.value.length - 1);
+          err.nif.invalidFormat = false;
+          return letterValue.toUpperCase() === 'TRWAGMYFPDXBNJZSQVHLCKE'.charAt(numberValue % 23) ? null : err;
+      } else { return err; }
+  };
+}
+@Directive({
+  selector: '[nif][formControlName],[nif][formControl],[nif][ngModel]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: NIFValidatorDirective, multi: true }]
+})
+export class NIFValidatorDirective implements Validator {
+  validate(control: AbstractControl): ValidationErrors | null {
+      return NIFValidator()(control);
+  }
+}
+@Directive({
+  selector: '[type][formControlName],[type][formControl],[type][ngModel]',
+  providers: [
+      { provide: NG_VALIDATORS, useExisting: forwardRef(() => TypeValidatorDirective), multi: true }
+  ]
+})
+export class TypeValidatorDirective implements Validator {
+  constructor(private elem: ElementRef) { }
+  validate(control: AbstractControl): ValidationErrors | null {
+      const valor = control.value;
+      if (valor) {
+        const dom = this.elem.nativeElement;
+        if (dom.validity) { // dom.checkValidity();
+          return dom.validity.typeMismatch ? { 'type': dom.validationMessage } : null;
+        }
+      }
+      return null;
+  }
+}
